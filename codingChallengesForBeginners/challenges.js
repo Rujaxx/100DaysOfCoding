19e6245f97d012221c6cef1a1fab06b4fd4119ea
Rujaxx <shaikhrujan@gmail.com>
2021-09-09 22:00:46 +0530

    day 8

--- codingChallengesForBeginners/challenges.js
+++ codingChallengesForBeginners/challenges.js
@@ -384,3 +384,79 @@ function isPrime(n) {
 }
 console.log(primeSum(100))
 
+// Coding challenge #29: Print the distance between the first 100 prime numbers
+function printDistances(n)
+{
+    let lastPrime = 2;
+    let i = lastPrime + 1;
+    let foundPrimes = 1;
+    
+    while(foundPrimes < n)
+    {
+        if (isPrime(i))
+        {
+            console.log(i - lastPrime, "\t", i, " - ", lastPrime);
+
+            foundPrimes++;
+            lastPrime = i;
+        }
+        
+        i++;
+    }
+}
+
+function isPrime(n) {
+    if(n <= 1){
+        return false
+    }else{
+        let maxDiv = Math.sqrt(n)
+        for( let i = 2; i <= maxDiv; i++ ){
+            if(n % i === 0) return false
+        }
+        return true
+    }
+}
+console.log(printDistances(100))
+
+// Coding challenge #30-a: Create a function that will add two positive numbers of indefinite size.
+//  The numbers are received as strings and the result should be also provided as string.
+
+function add(sNumber1, sNumber2)
+{
+    let s = "";
+    let carry = 0;
+    
+    let maxSize = Math.max(sNumber1.length, sNumber2.length);
+    
+    for(let i = 0; i < maxSize; i++)
+    {
+        let digit1 = digitFromRight(sNumber1, i);
+        let digit2 = digitFromRight(sNumber2, i);
+        
+        let sum = digit1 + digit2 + carry;
+        let digitSum = sum % 10;
+        carry = sum >= 10 ? 1 : 0;
+        
+        s = digitSum.toString() + s;
+    }
+    
+    if (carry > 0)
+        s = carry + s;
+    
+    return s;
+}
+
+function digitFromRight(s, digitNo)
+{
+    if (digitNo >= s.length)
+        return 0;
+        
+    let char = s[ s.length - 1 - digitNo ];
+    return parseInt(char);
+}
+
+let n1 = "2909034221912398942349";
+let n2 = "1290923909029309499";
+let sum = add(n1, n2);
+
+console.log(n1, "\n", n2, "\n", sum);
