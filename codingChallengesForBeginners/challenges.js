453233c94aa7a10837b1ae101dbfd5bb72f0b49d
Rujaxx <shaikhrujan@gmail.com>
2021-10-10 23:51:28 +0530

    day 30

--- codingChallengesForBeginners/challenges.js
+++ codingChallengesForBeginners/challenges.js
@@ -1496,3 +1496,25 @@ function findPeak(arr,n){
 var arr = [1, 3, 20, 4, 1, 0];
 var n = arr.length;
 findPeak(arr,n)
+
+//Given an array of integers and a positive integer k, determine the number of (i,j) pairs where i<j and ar[i]+ar[j] is divisible by k . 
+
+
+function divisibleSumPairs(n, k, ar) {
+    // Write your code here
+    let counter = 0;
+    for (let i = 0; i < n; i++) {
+        ar.slice(i + 1).filter((item) => {
+            if ((item + ar[i]) % k === 0) {
+                counter++
+            }
+        })
+    }
+    return counter;
+}
+
+let ar = [1,2,3,4,5,6]
+let k = 5
+let n = ar.length
+
+divisibleSumPairs(n,k,ar)
