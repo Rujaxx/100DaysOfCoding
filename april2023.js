8059cc930e47732496bd93739bb2a46090ede057
Rujaxx <shaikhrujan@gmail.com>
2023-04-10 23:39:25 +0530

    find index of

--- april2023.js
+++ april2023.js
@@ -116,17 +116,24 @@ let sortCount = (arr) =>{
         let l = x.length
         let count = 0
         for(let i=0; i < l; i++){
-            if(x ==l){
+            if(i == l){
                 break;
             }
-          if(x[i]<x[i+1]){
-            count++
-          }else{
-            count--
-          }  
+            
         }
       console.log(count)
     }
 }
 
 sortCount(["hello", "geeks"])
+
+
+
+
+// An sorted array of integers was rotated an unknown number of times.
+
+// Given such an array, find the index of the element in the array in faster than linear time. If the element doesn't exist in the array, return null.
+
+// For example, given the array [13, 18, 25, 2, 8, 10] and the element 8, return 4 (the index of 8 in the array).
+
+// You can assume all the integers in the array are unique.
